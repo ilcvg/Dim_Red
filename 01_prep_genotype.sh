9fdf0024f50cfce4ba3c268adac824468482297c
ilcvg <rb3@nibmg.ac.in>
2024-11-12 16:05:45 +0530

    01_prep_genotype.sh

---
+++ 01_prep_genotype.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script is written for preparing the genotype matrix to run dimensionality reduction >
+
+GENOTYPE="hgdp_chr21"
+
+# 0)
+plink2\
+ --pfile ${GENOTYPE}\
+ --make-bed\
+ --max-alleles 2\
+ --out ${GENOTYPE}
+
+
+# 1) LD pruning
+plink \
+ --bfile ${GENOTYPE} \
+ --indep-pairwise 50 5 0.2 \
+ --maf 0.01 \
+ --hwe 1E-6 \
+ --out ${GENOTYPE}
+
+plink \
+ --bfile ${GENOTYPE} \
+ --extract ${GENOTYPE}.prune.in \
+ --recode A \
+ --out ${GENOTYPE}.pruned
+
+# 2) make genotype matrix
+cat ${GENOTYPE}.pruned.raw | cut -d " " -f7- | awk 'NR>1{print}' > ${GENOTYPE}.pruned.geno.>
